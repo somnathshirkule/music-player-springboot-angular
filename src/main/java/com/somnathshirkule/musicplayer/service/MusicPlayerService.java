b60cb979a9c475b06d9c7023c55132f588b15cf7
unknown <shirkule.somnath@gmail.com>
2022-10-12 23:17:31 +0530

    added changes for heroku deployment

--- src/main/java/com/somnathshirkule/musicplayer/service/MusicPlayerService.java
+++ src/main/java/com/somnathshirkule/musicplayer/service/MusicPlayerService.java
@@ -3,13 +3,12 @@ package com.somnathshirkule.musicplayer.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,21 +16,44 @@ public class MusicPlayerService {
 	Map<Integer, File> musicFiles = new HashMap<>();
 	Map<Integer, String> musicFileNames = new HashMap<>();
 	
-	@Autowired
-	ResourceLoader resourceLoader;
 	
-	private void loadMusicFiles() {
-		File folder;
-		File[] listOfFiles = null;
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		folder = new File(classloader.getResource("musicfolder").getPath());
-		listOfFiles = folder.listFiles();
-		var counter = 0;
-		for (File file : listOfFiles) {
-			int index = counter++;
-			musicFiles.put(index, file);
-			musicFileNames.put(index, file.getName());
+	
+	private static final Logger log = Logger.getAnonymousLogger();  
+	
+	private void loadMusicFiles(){
+		URL url = MusicPlayerService.class.getResource("resources");
+		log.info("2");
+		log.info("URL::" + url);
+		if (url == null) {
+		     // error - missing folder
+		} else {
+		    File dir = null;
+			try {
+				dir = new File(url.toURI());
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			var counter = 0;
+		    for (File nextFile : dir.listFiles()) {
+		        // Do something with nextFile
+		    	int index = counter++;
+		    	musicFiles.put(index, nextFile);
+				musicFileNames.put(index, nextFile.getName());
+		    	log.info(nextFile.getName());
+		    }
 		}
+//		File folder;
+//		File[] listOfFiles = null;
+//		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+//		folder = new File(classloader.getResource("musicfolder").getPath());
+//		listOfFiles = folder.listFiles();
+//		var counter = 0;
+//		for (File file : listOfFiles) {
+//			int index = counter++;
+//			musicFiles.put(index, file);
+//			musicFileNames.put(index, file.getName());
+//		}
 	}
 
 	@SuppressWarnings("resource")
