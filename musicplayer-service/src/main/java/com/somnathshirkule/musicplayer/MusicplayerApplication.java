8af50fffbb02a2cfe5d575451f566c1c42895252
unknown <shirkule.somnath@gmail.com>
2022-11-06 12:52:12 +0530

    added angular project

--- musicplayer-service/src/main/java/com/somnathshirkule/musicplayer/MusicplayerApplication.java
+++ musicplayer-service/src/main/java/com/somnathshirkule/musicplayer/MusicplayerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicplayerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(MusicplayerApplication.class, args);
 	}
 
